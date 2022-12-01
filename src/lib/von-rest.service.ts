0a298026b618ae87c77bed11aec4ec4411dd096a
Luis García Castro <luis.garcia@von-ds.com>
2022-11-30 20:57:25 -0500

    Adjust rest service for files operation

--- src/lib/von-rest.service.ts
+++ src/lib/von-rest.service.ts
@@ -3,31 +3,47 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { map, share, take } from 'rxjs/operators';
 import { VonHttpOptionsModel } from './model/von-http-options.model';
+import {
+  DeleteParams,
+  GenericParams,
+  GetParams,
+  HeaderParams,
+  PostParams,
+  PutParams,
+} from './model/von-rest-definition.type';
 
 export abstract class VonRestService {
+  constructor(protected http: HttpClient, protected sanitizer: DomSanitizer) {}
 
-  constructor(
-    protected http: HttpClient,
-    protected sanitizer: DomSanitizer
-  ) { }
-
-  private setHeaders = (contentType: string = 'application/json'): HttpHeaders => {
+  protected setHeaders = (headerParams?: HeaderParams): HttpHeaders => {
     return new HttpHeaders({
-      'Content-Type': 'application/json',
-      Accept: contentType,
-      'Access-Control-Allow-Origin': '*'
+      'Content-Type': headerParams?.contentType ?? 'application/json',
+      Accept: headerParams?.accept ?? 'application/json',
+      'Access-Control-Allow-Origin': '*',
     });
-  }
+  };
 
-  private setOptions = (params?: { [key: string]: any }): VonHttpOptionsModel => {
+  protected setOptions = (headerParams?: HeaderParams): VonHttpOptionsModel => {
     return {
-      headers: this.setHeaders(),
-      params,
-      withCredentials: true
+      headers: this.setHeaders(headerParams),
+      params: headerParams?.params,
+      withCredentials: true,
     };
-  }
+  };
 
-  private setUrlParams = (url: string, params?: { [key: string]: any }) => {
+  protected setOptionsForFile = (
+    headerParams?: HeaderParams
+  ): VonHttpOptionsModel => {
+    return {
+      ...this.setOptions(headerParams?.params),
+      headers: new HttpHeaders({
+        Accept: headerParams?.accept ?? 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      }),
+    };
+  };
+
+  protected setUrlParams = (url: string, params?: GenericParams) => {
     if (params != null) {
       for (const p in params) {
         if (p != null) {
@@ -36,87 +52,104 @@ export abstract class VonRestService {
       }
     }
     return url;
-  }
+  };
 
-  authenticate = (url: string, username: string, password: string): Observable<any> => {
+  protected authenticate = (
+    url: string,
+    username: string,
+    password: string
+  ): Observable<any> => {
     const authorization = btoa(`${username}:${password}`);
     const headers = new HttpHeaders({
       authorization: `Basic ${authorization}`,
     });
-    return this.http.get(url, {
-      headers,
-      withCredentials: true
-    }).pipe(
-      take(1),
-      share()
-    );
-  }
-
-  logout = (url: string): Observable<any> => {
-    return this.http.delete<string>(url, this.setOptions()).pipe(
-      take(1),
-      share()
-    );
-  }
-
-  get = <T = any>(url: string, urlParams?: any, queryParams?: any): Observable<T> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.get<T>(url, this.setOptions(queryParams));
-  }
-
-  delete = <T = any>(url: string, urlParams?: any, queryParams?: any): Observable<T> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.delete<T>(url, this.setOptions(queryParams));
-  }
-
-  post = <T = any>(url: string, body: any, urlParams?: any, queryParams?: any): Observable<T> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.post<T>(url, body, this.setOptions(queryParams));
-  }
-
-  put = <T = any>(url: string, body: any, urlParams?: any, queryParams?: any): Observable<T> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.put<T>(url, body, this.setOptions(queryParams));
-  }
-
-  patch = <T = any>(url: string, body: any, urlParams?: any, queryParams?: any): Observable<T> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.patch<T>(url, body, this.setOptions(queryParams));
-  }
-
-  private setFileOptions = (contentType: string, queryParams?: { [key: string]: any }): VonHttpOptionsModel => {
-    return {
-      ...this.setOptions(queryParams),
-      headers: this.setHeaders(contentType),
-      responseType: 'blob' as 'json'
-    };
-  }
-
-  getFile = (contentType: string, url: string, urlParams?: any, queryParams?: any): Observable<SafeResourceUrl> => {
-    url = this.setUrlParams(url, urlParams);
-    return this.http.get(url, this.setFileOptions(contentType, queryParams)).pipe(
-      map((res: any) => {
-        const fileBlob = new Blob([res], { type: contentType });
-        const objUrl = URL.createObjectURL(fileBlob);
-        const sanitized = this.sanitizer.bypassSecurityTrustResourceUrl(objUrl);
-        return sanitized;
+    return this.http
+      .get(url, {
+        headers,
+        withCredentials: true,
       })
-    );
-  }
+      .pipe(take(1), share());
+  };
 
-  postFile = (contentType: string, url: string, body: any, urlParams?: any, queryParams?: any): Observable<any> => {
+  protected get = <R = any>({
+    url,
+    urlParams,
+    params,
+  }: GetParams): Observable<R> => {
     url = this.setUrlParams(url, urlParams);
-    return this.http.post(url, body, this.setFileOptions(contentType, queryParams));
-  }
+    return this.http.get<R>(url, this.setOptions(params));
+  };
 
-  putFile = (contentType: string, url: string, body: any, urlParams?: any, queryParams?: any): Observable<any> => {
+  protected file = ({
+    url,
+    urlParams,
+    params,
+  }: GetParams): Observable<SafeResourceUrl> => {
     url = this.setUrlParams(url, urlParams);
-    return this.http.put(url, body, this.setFileOptions(contentType, queryParams));
-  }
+    return this.http
+      .get(url, {
+        ...this.setOptionsForFile(params),
+        responseType: 'blob' as 'json',
+      })
+      .pipe(
+        map((res: any) => {
+          const fileBlob = new Blob([res], { type: params?.contentType });
+          const objUrl = URL.createObjectURL(fileBlob);
+          const sanitized =
+            this.sanitizer.bypassSecurityTrustResourceUrl(objUrl);
+          return sanitized;
+        })
+      );
+  };
 
-  patchFile = (contentType: string, url: string, body: any, urlParams?: any, queryParams?: any): Observable<any> => {
+  protected delete = <R = any>({
+    url,
+    urlParams,
+    params,
+  }: DeleteParams): Observable<R> => {
     url = this.setUrlParams(url, urlParams);
-    return this.http.patch(url, body, this.setFileOptions(contentType, queryParams));
-  }
+    return this.http.delete<R>(url, this.setOptions(params));
+  };
 
+  protected post = <R = any, B = any | FormData>({
+    url,
+    body,
+    urlParams,
+    params,
+  }: PostParams<B>): Observable<R> => {
+    url = this.setUrlParams(url, urlParams);
+    let options =
+      body instanceof FormData
+        ? this.setOptionsForFile(params)
+        : this.setOptions(params);
+    return this.http.post<R>(url, body, options);
+  };
+
+  protected put = <R = any, B = any | FormData>({
+    url,
+    body,
+    urlParams,
+    params,
+  }: PutParams<B>): Observable<R> => {
+    url = this.setUrlParams(url, urlParams);
+    let options =
+      body instanceof FormData
+        ? this.setOptionsForFile(params)
+        : this.setOptions(params);
+    return this.http.put<R>(url, body, options);
+  };
+
+  protected patch = <R = any, B = any | FormData>({
+    url,
+    body,
+    urlParams,
+    params,
+  }: PutParams<B>): Observable<R> => {
+    url = this.setUrlParams(url, urlParams);
+    let options =
+      body instanceof FormData
+        ? this.setOptionsForFile(params)
+        : this.setOptions(params);
+    return this.http.patch<R>(url, body, options);
+  };
 }
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,6 +1,6 @@
 export * from './lib/model/von-error-response.model';
 export * from './lib/model/von-error-rest-interceptor.model';
 export * from './lib/model/von-page-response.model';
-export * from './lib/model/von-page-response.model';
+export * from './lib/model/von-rest-definition.type';
 export * from './lib/von-rest-interceptor.service';
 export * from './lib/von-rest.service';
