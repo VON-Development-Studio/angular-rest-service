4458bcc6a2542b96b431081e8cf751786c9c9838
Luis Garcia Castro <luis.fernando.garcia.castro@gmail.com>
2026-01-09 19:35:18 -0500

    Refactor demo preview of the implementation

---
+++ projects/example/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+export const routes: Routes = [
+  // RouterModule.forRoot([
+  //   { path: '', component: AppComponent },
+  //   { path: '**', redirectTo: '/' },
+  // ]),
+];
